582616f648dda3a3e1f3238070bf536a30bb9d1d
CEREN SUSUZ <ceren199704@hotmail.com>
2021-03-11 00:07:31 +0300

    cors + code refactoring

--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace WebAPI.Controllers
@@ -24,6 +25,7 @@ namespace WebAPI.Controllers
         [HttpGet("getall")]
         public IActionResult GetAll()
         {
+            Thread.Sleep((2000));
             var result = _productService.GetAll();
             if (result.Success)
             {
