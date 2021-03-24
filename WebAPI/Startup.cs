f2a6cf078ae0ad9124c30971616f510e5e346ed8
CEREN SUSUZ <ceren199704@hotmail.com>
2021-03-24 19:39:41 +0300

    exceptionmiddleware + error

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -59,6 +59,8 @@ namespace WebAPI
                 app.UseDeveloperExceptionPage();
             }
 
+            app.ConfigureCustomExceptionMiddleware();
+
             app.UseCors(builder=>builder.WithOrigins("http://localhost:4200").AllowAnyHeader());
 
             app.UseHttpsRedirection();
